3ee80ed2108d6cec4a8aa95e9a8319e18f0ef985
Lucas de Sousa Assunção <lucasdesousa19@hotmail.com>
2022-07-14 14:32:26 +0000

    add express

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
-import axios from 'axios'
+import express from 'express'
 import cron from 'node-cron'
 import { BotController } from './controllers/BotController.js'
 
 
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        api: 'Automation node'
+    })
+})
 
 cron.schedule('*/5 * * * * *', () => {
     console.log('Teste cron')
 })
 
-cron.schedule('* * * * *', () => {
+cron.schedule('0/34 * * * *', () => {
     BotController.priceDay();
 })
+
+app.listen(process.env.PORT || 8000)
